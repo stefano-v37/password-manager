a75262a92e6562ac907fe6d40a789ff2286f269f
Stefano <s.vanin@outlook.it>
2020-11-01 19:55:20 +0100

    quick fix on my errors on git configuration

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="PasswordManager", # Replace with your own username
+    name="PasswordManager",
     version="0.1.0",
     author="Stefano Vanin",
     description="Python password manager",
